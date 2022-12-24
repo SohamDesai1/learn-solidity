c96bf8be2413aac76d25679be43fdbc21868cf6f
SohamDesai1 <sohamdesai123@gmail.com>
2022-12-25 00:43:36 +0530

    variables

---
+++ Var.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+contract MyContract{
+    function getValue() pure public returns ( uint) {
+        uint value = 1;
+        return value;
+    }
+}
+
